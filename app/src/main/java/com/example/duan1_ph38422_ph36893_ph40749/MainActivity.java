0c491bab59f7f83dbfe6cd0cc29aa03cdec5f316
nam1112 <namden9399@gmail.com>
2023-11-14 20:06:30 +0700

    nam commentt

--- app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/MainActivity.java
+++ app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // hieu comment
-        // nam comment
+        // nam commentt
     }
 }
