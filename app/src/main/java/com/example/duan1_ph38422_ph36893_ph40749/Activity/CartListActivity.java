d9419b57ca2b49315e8ebf05e1f57520f1b92b30
Admin <hieunvph38422@fpt.edu.vn>
2023-11-22 00:30:57 +0700

    21/11/2023 Minh: viewholder_category 21/11/2023 Minh: CartListActivity

--- app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/Activity/CartListActivity.java
+++ app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/Activity/CartListActivity.java
@@ -1,18 +1,37 @@
 package com.example.duan1_ph38422_ph36893_ph40749.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.duan1_ph38422_ph36893_ph40749.R;
 
 public class CartListActivity extends AppCompatActivity {
-
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewList;
+    //private ManagementCart managementCart;
+    TextView totalFeeTxt, taxTxt, deliveryTxt,totalTxt, emptyTxt;
+    private double tax;
+    private ScrollView scrollView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart_list);
 
+     //   managementCart=new ManagementCart(this);
+    initView();
+    }
+
+    private void initView() {
+        recyclerViewList=findViewById(R.id.recyclerView);
+        totalFeeTxt=findViewById(R.id.totalFeeTxt);
+        taxTxt=findViewById(R.id.taxTxt);
+        deliveryTxt=findViewById(R.id.deliveryTxt);
+        totalTxt=findViewById(R.id.totalTxt);
+        emptyTxt=findViewById(R.id.)
 
     }
 }
