5573958f308148744a0ad0ecb2b8589ec5bc9a76
nam1112 <namden9399@gmail.com>
2023-11-28 22:11:10 +0700

    28/11/2023 nam làm  Product

---
+++ app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/Fragment/Product_frg.java
@@ -0,0 +1,25 @@
+package com.example.duan1_ph38422_ph36893_ph40749.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duan1_ph38422_ph36893_ph40749.R;
+
+
+public class Product_frg extends Fragment {
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_product_frg, container, false);
+    }
+}
