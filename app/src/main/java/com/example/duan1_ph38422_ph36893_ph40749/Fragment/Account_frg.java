e733c453723ed31e83b370f39fd3ee6d37c5dcd8
hieu1309 <hieucudau@gmail.com>
2023-11-28 23:40:14 +0700

    28/11 hieu lam giao dien account

---
+++ app/src/main/java/com/example/duan1_ph38422_ph36893_ph40749/Fragment/Account_frg.java
@@ -0,0 +1,30 @@
+package com.example.duan1_ph38422_ph36893_ph40749.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.duan1_ph38422_ph36893_ph40749.R;
+
+
+public class Account_frg extends Fragment {
+
+
+
+    public Account_frg() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account_frg, container, false);
+    }
+}
